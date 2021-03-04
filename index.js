c50b46d3f4d3826cf4fa85b257e6f3a7826ac4ed
pa3kdow1c <patrik.dovicovic@gmail.com>
2021-03-04 15:17:44 +0100

    pridal som novu vetvu settings

---
+++ index.js
@@ -0,0 +1,23 @@
+// var submit = document.getElementById("play");s
+
+let gameIsRunning = true;
+let WallKillSnake
+let showTile = true;
+const fps = 10 ;
+const tileSize = 60;
+let score = 0;
+let snakeLength = 4;
+
+// submit.onclick = function(){
+//     const ul = document.querySelectorAll(".ul");
+//         for (var i = 0; i < ul.length; i++) {
+//             const checkbox = ul[i].querySelectorAll(".checkbox_info");
+//             if (checkbox[0].checked === true){
+//                 WallKillSnake = true;
+//             }
+//             else {
+//                 WallKillSnake = false
+//             }
+//             console.log(WallKillSnake)
+//         }
+// }
